381fa55bf02c0c2f7e99084f3298801ce7a79847
Guiki18 <foxzillapt17@gmail.com>
2022-06-24 16:12:48 +0100

    c

--- install.sh
+++ install.sh
@@ -4,4 +4,22 @@ sudo apt update &>/dev/null && echo "List of apt packages successfully updated" 
 sudo apt-get install openjdk-17-jre-headless &>/dev/null && echo "Java v17 has been successfully installed" || echo "Failed to install Java v17."
 JAVA_VER=$(java -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
 
-[ "$JAVA_VER" -ge 15 ] && echo "Java v17 Successfully Configured" || echo "Failed to configure Java"
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" > "1.5" ]]; then
+        echo version is more than 1.5
+    else         
+        echo version is less than 1.5
+    fi
+fi
