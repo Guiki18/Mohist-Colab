732fd052ab0819967ae702e2f69c39bcacc2ce8a
Guiki18 <foxzillapt17@gmail.com>
2022-06-24 16:08:19 +0100

    A

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 sudo apt update &>/dev/null && echo "List of apt packages successfully updated" || echo "The apt list package update failed. You you might receive stale packages"
 sudo apt-get install openjdk-17-jre-headless &>/dev/null && echo "Java v17 has been successfully installed" || echo "Failed to install Java v17."
+JAVA_VER=$(java -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
 
