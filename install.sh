fb15b915aa6a9f46ad47d82c15f621a407fa0574
Guiki18 <foxzillapt17@gmail.com>
2022-06-24 16:11:47 +0100

    b

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo apt update &>/dev/null && echo "List of apt packages successfully updated" 
 sudo apt-get install openjdk-17-jre-headless &>/dev/null && echo "Java v17 has been successfully installed" || echo "Failed to install Java v17."
 JAVA_VER=$(java -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
 
+[ "$JAVA_VER" -ge 15 ] && echo "Java v17 Successfully Configured" || echo "Failed to configure Java"
